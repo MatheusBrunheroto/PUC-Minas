e0e740126ef7f694f1a404a82359c015249fb137
Matheus <67561023+MatheusBrunheroto@users.noreply.github.com>
2023-05-19 13:36:38 -0300

    União dos 3
    site amador porém feito com amor

---
+++ Software/1º Período/Seminários I/4 - HTML CSS e JS/functions.js
@@ -0,0 +1,7 @@
+function ampliar_normal(x){
+    if(x == 0){
+        document.getElementById("fonte").style.fontSize = "22px";
+    } else {
+        document.getElementById("fonte").style.fontSize = "16px";
+    }
+}
